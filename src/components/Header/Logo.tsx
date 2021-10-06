2f07044786869d34f900f92572a9f218226c443d
Bernardo Bevilaqua <bernardo.bfg@hotmail.com>
2021-10-06 11:02:17 -0300

    add responsiveness at header component

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,15 +2,15 @@ import { Text } from "@chakra-ui/react";
 export function Logo() {
   return (
     <Text
-    fontSize="3xl"
-    fontWeight="bold"
-    letterSpacing="tight"
-    w="64"
+      fontSize={["2xl", "3xl"]}
+      fontWeight="bold"
+      letterSpacing="tight"
+      w="64"
 
->
-    dashgo
-    <Text color="pink.500" as="span" ml="1">.</Text>
-</Text>
+    >
+      dashgo
+      <Text color="pink.500" as="span" ml="1">.</Text>
+    </Text>
 
   )
 }
